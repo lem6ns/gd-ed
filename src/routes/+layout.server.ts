ca782acd084790538fc729cfcb3b22a67bb99ba1
cloud <62519659+lem6ns@users.noreply.github.com>
2023-02-02 19:32:30 -0700

    fix: unneeded complexity

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
-	if (!session?.user) throw redirect(303, "/auth/signin");
-    if (!session.user.inServer) throw redirect(303, "/signout")
+	if (!(session?.user?.inServer)) throw redirect(303, "/auth/signin");
 	return { session };
 };
