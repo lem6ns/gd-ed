20d5db57ec0c7b1b32441775efa2b1ed2655f5ad
cloud <62519659+lem6ns@users.noreply.github.com>
2023-02-02 19:44:08 -0700

    feat: example .env configuration

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,6 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
-	if (!(session?.user?.inServer)) throw redirect(303, "/auth/signin");
+	if (!session?.user?.inServer) throw redirect(303, "/auth/signin");
 	return { session };
 };
